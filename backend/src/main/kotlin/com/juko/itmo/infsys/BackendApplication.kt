8e04fdc1ae107fc45c3726b81025eea24c3cc475
XyL1GaN4eG <trolloguy@mail.ru>
2025-09-28 20:51:32 +0300

    rename repo

--- backend/src/main/kotlin/com/juko/itmo/infsys/BackendApplication.kt
+++ backend/src/main/kotlin/com/juko/itmo/infsys/BackendApplication.kt
@@ -1,4 +1,4 @@
-package com.juko.itmo.`is`
+package com.juko.itmo.infsys
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
--- backend/src/test/kotlin/com/juko/itmo/infsys/BackendApplicationTests.kt
+++ backend/src/test/kotlin/com/juko/itmo/infsys/BackendApplicationTests.kt
@@ -1,4 +1,4 @@
-package com.juko.itmo.`is`
+package com.juko.itmo.infsys
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest

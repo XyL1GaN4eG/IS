d94747e4d6d106214c399cf37a0d9859b3240058
XyL1GaN4eG <trolloguy@mail.ru>
2025-09-29 21:17:15 +0300

    Style: add trailing comma🤓

--- backend/src/main/kotlin/com/juko/itmo/infsys/service/PersonService.kt
+++ backend/src/main/kotlin/com/juko/itmo/infsys/service/PersonService.kt
@@ -3,13 +3,13 @@ package com.juko.itmo.infsys.service
 import com.juko.itmo.infsys.data.entity.PersonEntity
 import com.juko.itmo.infsys.data.model.dto.Person
 import com.juko.itmo.infsys.data.repository.PersonRepository
+import com.juko.itmo.infsys.service.abstraction.CrudService
 import com.juko.itmo.infsys.util.PersonMapper
-import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Service
 
 @Service
 class PersonService(
     repository: PersonRepository,
-    mapper: PersonMapper
+    mapper: PersonMapper,
 ) : CrudService<Person, PersonEntity>(repository, mapper) {
 }
